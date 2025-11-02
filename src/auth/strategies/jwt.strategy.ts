4eff675f51cb5c86a12b2c6fa65f44ec08582272
Ebrahim Mansour <ebrahimmansour2001@gmail.com>
2025-11-02 21:17:47 +0200

    add basic routing and auth context

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.access_token;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    if (!payload.sub) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    return { userId: payload.sub };
+  }
+}
+
